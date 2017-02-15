10be8c4c4215cb4a7b9cca9cadd329a4f9e80ea7
ernes <ernes@DESKTOP-BMBO47R>
2017-02-15 00:26:48 -0800

    notes on bigger

--- CS485_Student_Examples/01_CopyConstructorAndAssignmentOp/bigger.h
+++ CS485_Student_Examples/01_CopyConstructorAndAssignmentOp/bigger.h
@@ -16,17 +16,21 @@ public:
 
   bigger ();
 
-  /*explicit*/ bigger (int x);
+  /*explicit*/ bigger (int x); //* dont do explicit will give an error to b7 
+                               // because it cant be done inplicitly anymore 
+                               // unless static cast 1 as bigger*
+
+                               // ctor, cctor, dtor, and operator will be built automatically
 
   ~bigger ();
 
-  bigger (const bigger &rcData);
+  bigger (const bigger &rcData) = delete; // will prevent from making copies of objects
 
-  bigger& operator= (const bigger &rcData);
-  
+  bigger& operator= (const bigger &rcData) = delete; // control compiler
+
 private:
   int mData = 0;
   int mID;
 
-  int getID() const;
+  int getID () const;
 };
